da48b3469b084878d906e670ceeee6092933bfbb
Marcio de Oliveira da Silva <marcio.oliveira@klarna.com>
2017-02-07 21:48:08 +0100

    Backoff and retry

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var axios = require('axios');
-var parser = require('xml2json');
+var axios = require('axios')
+var parser = require('xml2json')
+
+const REQUEST_TIMEOUT_MS = 10000
+const BACKOFF_TIME_MS = 2000
+const MAX_RETRIES = 10
 
 const axiosInstance = axios.create({
   baseURL: 'https://www.boardgamegeek.com/xmlapi2/',
-  timeout: 5000,
+  timeout: REQUEST_TIMEOUT_MS,
   headers: {
     'Accept': 'text/xml',
     'Content-Type': 'text/xml'
@@ -14,16 +18,35 @@ axiosInstance.interceptors.response.use(function (response) {
   return parser.toJson(response.data, { object: true })
 })
 
-module.exports = function(path, parameters = {}) {
-
-  const queryString = Object.keys(parameters).reduce(function (prev, key) {
+function buildQueryString (parameters) {
+  return Object.keys(parameters).reduce(function (prev, key) {
     let parameter = ''
-
     if (prev !== '?') {
       parameter += '&'
     }
     return `${prev}${parameter}${key}=${parameters[key]}`
   }, '?')
+}
 
+function doRequest (path, queryString, retries = 0) {
   return axiosInstance.get(`${path}${queryString}`)
+          .then(function (response) {
+            if (response.message && response.message.includes('processed')) {
+              if (retries === MAX_RETRIES) {
+                throw new Error('Maximum retries reached')
+              }
+              return new Promise(function (resolve, reject) {
+                setTimeout(function () {
+                  resolve(doRequest(path, queryString, ++retries))
+                }, BACKOFF_TIME_MS)
+              })
+            }
+            return response
+          })
+}
+
+module.exports = function(path, parameters = {}) {
+  const queryString = buildQueryString(parameters)
+
+  return doRequest(path, queryString)
 }
--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
 var bgg = require('../');
 
-bgg('collection', { username: 'marcio_os' }).then((response) => console.log(response)).catch((error) => console.log(error));
+bgg('collection', { username: 'marcio_os' })
+  .then(function (response) { console.log('Got response: ', response) })
+  .catch(function (error) { console.log('Caught error: ', error) })
